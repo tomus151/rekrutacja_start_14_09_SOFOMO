84b8f0b5139cbd8e2d813328dd039c14eccb0c05
tommar <tomasz.markowski@agora.pl>
2022-09-16 19:44:33 +0200

    feat: to deploy

--- src/api/ipStackApi.js
+++ src/api/ipStackApi.js
@@ -5,7 +5,7 @@ const ipStackInfoApi = IP_API.injectEndpoints({
     endpoints: (build) => ({
         getIpStack: build.mutation({
             query: (param) => ({
-                url: `${IP_STACK_URL}${param}?access_key=ITWgTcpHT8JDcS5MaBqC&format=1`,
+                url: `${IP_STACK_URL}${param}?access_key=a850be7a32ad5242dbef353d4eabc6f6&format=1`,
                 method: "GET"
             })
         })
